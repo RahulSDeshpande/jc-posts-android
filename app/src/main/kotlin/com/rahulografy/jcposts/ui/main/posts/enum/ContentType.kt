786b60fd10853a19a17182ac55e944974eb9538d
AndroCurious <rahul.deshpande92@gmail.com>
2020-10-17 02:39:59 +0400

    :fire: Create listeners & content type enum

---
+++ app/src/main/kotlin/com/rahulografy/jcposts/ui/main/posts/enum/ContentType.kt
@@ -0,0 +1,8 @@
+package com.rahulografy.jcposts.ui.main.posts.enum
+
+annotation class ContentType {
+    companion object {
+        const val POSTS = "POSTS"
+        const val FAVORITES = "FAVORITES"
+    }
+}
