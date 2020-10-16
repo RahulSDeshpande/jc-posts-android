56d93f75da32b34c956a5af436233b6a11b8a7b2
AndroCurious <rahul.deshpande92@gmail.com>
2020-10-17 02:38:17 +0400

    :fire: Initialize Stetho lib in App

--- app/src/main/kotlin/com/rahulografy/jcposts/App.kt
+++ app/src/main/kotlin/com/rahulografy/jcposts/App.kt
@@ -2,6 +2,7 @@ package com.rahulografy.jcposts
 
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.facebook.stetho.Stetho
 import com.rahulografy.jcposts.di.component.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -9,6 +10,13 @@ import dagger.android.DaggerApplication
 open class App : DaggerApplication() {
 
     private lateinit var androidInjector: AndroidInjector<out DaggerApplication>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Init Stetho
+        Stetho.initializeWithDefaults(this)
+    }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
