e40786b0f0692f0ca4eba980683a9398f52c0ad0
AndroCurious <rahul.deshpande92@gmail.com>
2020-10-16 13:58:28 +0400

    Create base view-model & activity

---
+++ app/src/main/kotlin/util/ViewModelFactory.kt
@@ -0,0 +1,22 @@
+package util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rahulografy.jcposts.di.ApplicationScoped
+import javax.inject.Inject
+import javax.inject.Provider
+
+@ApplicationScoped
+class ViewModelFactory @Inject constructor(
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass as Class<ViewModel>]
+            ?: creators.entries.firstOrNull { (c, _) -> modelClass.isAssignableFrom(c) }?.value
+            ?: throw IllegalArgumentException("Unknown model class $modelClass")
+
+        return creator.get() as T
+    }
+}
