99a8d8c9208b36dd6f0c9a088f69e79644aa3944
AndroCurious <rahul.deshpande92@gmail.com>
2020-10-17 02:45:29 +0400

    :fire: :fire: :fire: Create PostsFragment + integration with posts data layer via view-model

---
+++ app/src/main/kotlin/com/rahulografy/jcposts/ui/main/posts/PostsFragment.kt
@@ -0,0 +1,86 @@
+package com.rahulografy.jcposts.ui.main.posts
+
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.rahulografy.jcposts.BR
+import com.rahulografy.jcposts.R
+import com.rahulografy.jcposts.data.source.local.posts.model.PostEntity
+import com.rahulografy.jcposts.databinding.FragmentPostsBinding
+import com.rahulografy.jcposts.ui.base.view.BaseFragment
+import com.rahulografy.jcposts.ui.main.posts.enum.ContentType.Companion.POSTS
+import com.rahulografy.jcposts.ui.main.posts.listener.PostEventListener
+import com.rahulografy.jcposts.util.ext.show
+import kotlinx.android.synthetic.main.fragment_posts.*
+
+class PostsFragment :
+    BaseFragment<FragmentPostsBinding, PostsFragmentViewModel>(),
+    PostEventListener {
+
+    companion object {
+        private const val ARG_CONTENT_TYPE = "ARG_CONTENT_TYPE"
+
+        fun init(contentType: String): PostsFragment {
+            val productFragment = PostsFragment()
+            val bundle = Bundle()
+            bundle.putString(ARG_CONTENT_TYPE, contentType)
+            productFragment.arguments = bundle
+            return productFragment
+        }
+    }
+
+    override val layoutRes get() = R.layout.fragment_posts
+
+    override var viewModelClass = PostsFragmentViewModel::class.java
+
+    override val bindingVariable = BR.viewModel
+
+    private var contentType = POSTS
+
+    override fun initUi() {
+        contentType = arguments?.getString(ARG_CONTENT_TYPE) ?: POSTS
+
+        if (isAppOnline()) {
+            viewModel.getPosts()
+        }
+    }
+
+    override fun initObservers() {
+        viewModel.apply {
+            postsMutableLiveData
+                .observe(
+                    lifecycleOwner = this@PostsFragment,
+                    observer = Observer { posts ->
+                        initPostsRecyclerView(posts = posts)
+                    }
+                )
+        }
+    }
+
+    private fun initPostsRecyclerView(posts: ArrayList<PostEntity>?) {
+        if (posts.isNullOrEmpty().not()) {
+            viewDataBinding.apply {
+                // TODO | SET DATA TO ADAPTER
+                executePendingBindings()
+            }
+
+            viewModel.postsObservableField.set(posts)
+
+            showPostsRecyclerView(show = true)
+        } else {
+            showPostsRecyclerView(show = false)
+        }
+    }
+
+    private fun showPostsRecyclerView(show: Boolean) {
+        rvPosts.show(show = show)
+        layoutNoData.show(show = show.not())
+    }
+
+    override fun onListItemClicked(postEntity: PostEntity) {
+        openCommentsFragment(postId = postEntity.id)
+    }
+
+    private fun openCommentsFragment(postId: Int?) {
+        // TODO | WIP
+    }
+}
