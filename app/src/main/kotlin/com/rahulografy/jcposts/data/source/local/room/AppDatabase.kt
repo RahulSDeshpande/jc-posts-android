092b00720aff1752ef7c79ef91141095fec27f89
AndroCurious <rahul.deshpande92@gmail.com>
2020-10-17 18:38:40 +0400

    Update app database, entities, repos & refine dirty flags

--- app/src/main/kotlin/com/rahulografy/jcposts/data/source/local/room/AppDatabase.kt
+++ app/src/main/kotlin/com/rahulografy/jcposts/data/source/local/room/AppDatabase.kt
@@ -1,14 +1,11 @@
 package com.rahulografy.jcposts.data.source.local.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.rahulografy.jcposts.data.source.local.comments.dao.CommentsDao
 import com.rahulografy.jcposts.data.source.local.comments.model.CommentEntity
 import com.rahulografy.jcposts.data.source.local.posts.dao.PostsDao
 import com.rahulografy.jcposts.data.source.local.posts.model.PostEntity
-import com.rahulografy.jcposts.util.Constants.Network.Db.DB_NAME
 
 @Database(
     entities = [
@@ -19,28 +16,6 @@ import com.rahulografy.jcposts.util.Constants.Network.Db.DB_NAME
     exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
-
-    @Volatile
-    private var appDatabase: AppDatabase? = null
-
-    @Synchronized
-    open fun getInstance(context: Context): AppDatabase? {
-        if (appDatabase == null) {
-            appDatabase = init(context = context)
-        }
-        return appDatabase
-    }
-
-    private fun init(context: Context): AppDatabase? {
-        return Room
-            .databaseBuilder(
-                context,
-                AppDatabase::class.java,
-                DB_NAME
-            )
-            .fallbackToDestructiveMigration()
-            .build()
-    }
 
     abstract fun postsDao(): PostsDao
 
