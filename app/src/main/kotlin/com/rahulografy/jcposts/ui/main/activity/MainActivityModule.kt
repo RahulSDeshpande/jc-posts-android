467b58097b0d502b3b48e820c6045c7bc073aa3a
AndroCurious <rahul.deshpande92@gmail.com>
2020-10-17 18:44:53 +0400

    Bind fragment & VM into Dagger

--- app/src/main/kotlin/com/rahulografy/jcposts/ui/main/activity/MainActivityModule.kt
+++ app/src/main/kotlin/com/rahulografy/jcposts/ui/main/activity/MainActivityModule.kt
@@ -2,6 +2,7 @@ package com.rahulografy.jcposts.ui.main.activity
 
 import com.rahulografy.jcposts.di.FragmentScoped
 import com.rahulografy.jcposts.ui.main.home.fragment.HomeFragment
+import com.rahulografy.jcposts.ui.main.postdetail.fragment.PostDetailFragment
 import com.rahulografy.jcposts.ui.main.posts.fragment.PostsFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -16,4 +17,8 @@ abstract class MainActivityModule {
     @FragmentScoped
     @ContributesAndroidInjector
     abstract fun bindPostsFragment(): PostsFragment
+
+    @FragmentScoped
+    @ContributesAndroidInjector
+    abstract fun bindPostDetailFragment(): PostDetailFragment
 }
